622a4e20520a25c4f16f19773a7482d14f5bb5b1
oskiman18 <oscar__180894@hotmail.com>
2020-07-03 09:39:37 -0300

    esrawsr

--- main.cpp
+++ main.cpp
@@ -11,80 +11,17 @@
 
 void main()
 {
-
+    int opc;
+    bool continuar=true;
     iniciar_allegro();
-    Mapa oMapa;
-    Pacman oPacman;
-//    posicion_pacman_x = oPacman.getPosXPac();
-//    posicion_pacman_y = oPacman.getPosYPac();
-    bool primera_vez = false;
+    opc=pantalla_inicial();
+    while(continuar)
+    switch (opc){
+    case 1: {jugar();}break;
+//    case 2: menu_puntaje();break;
+//    case 3: menu_idioma(); break;
+    case 0: continuar=false;}
 
-    Fantasma oFantasma1(TAM*12,TAM*10,0);
-    Fantasma oFantasma2(TAM*13,TAM*10,1);
-    Fantasma oFantasma3(TAM*14,TAM*10,2);
-    Fantasma oFantasma4(TAM*15,TAM*10,3);
-    Fantasma oFantasma5(TAM*16,TAM*10,4);
-//    Fantasma oFantasma6(TAM*12,TAM*11,1);
-
-
-    pantalla_inicial();
-    pantalla_elegir_skin();
-
-    play_midi(sountrack_stage_1,1);
-
-    while(!key[KEY_ESC] && !game_over){
-        if(!primera_vez){
-            //para pausar la pantalla al comienzo
-            pantalla_princio(&primera_vez,oMapa,oPacman);
-        }
-        dibujar_vidas_pacman(oPacman.getVidas());
-        dibujar_puntaje(puntaje);
-
-//        se_presiono_una_tecla(oMapa);
-        oPacman.mover_pacman();
-
-        if(oPacman.comidaGrande()){
-        oFantasma1.cambiarEstado();
-        oFantasma2.cambiarEstado();
-        oFantasma3.cambiarEstado();
-        oFantasma4.cambiarEstado();
-        oFantasma5.cambiarEstado();
-        }
-
-        oFantasma1.moverFantasma();
-        oFantasma2.moverFantasma();
-        oFantasma3.moverFantasma();
-        oFantasma4.moverFantasma();
-        oFantasma5.moverFantasma();
-
-//      imprime el pacman con la boca abierta
-        clear(buffer);
-        oPacman.Comer();
-        oPacman.imprimirPacmanComiendo();
-        oFantasma1.dibujarFantasma();
-        oFantasma2.dibujarFantasma();
-        oFantasma3.dibujarFantasma();
-        oFantasma4.dibujarFantasma();
-        oFantasma5.dibujarFantasma();
-//        oFantasma6.dibujarFantasma();
-        oMapa.imprimirMapa();
-
-        rest(125);
-
-
-        //imprime el pacman con la boca cerrada
-        clear(pacman);
-        oPacman.imprimirPacmanQuieto();
-        oMapa.imprimirMapa();
-        rest(125);
-        oMapa.imprimirMapa();
-      //  oPacman.restarVida();
-//        dibujar_vidas_pacman();
-
-        if(!oMapa.hayComida() || oPacman.getVidas() == 0){ //si no hay comida... termina el juego
-            game_over = true;
-        }
-    }
-destruir();
+    destruir();
 }
 END_OF_MAIN();
